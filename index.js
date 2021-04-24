5af4c982f3a871c4ee33f21fa97cc58f7e6e289a
Dawodu Oluwananumi <nanudawodu@gmail.com>
2021-04-24 03:19:51 -1100

    working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5554;
@@ -5,8 +6,7 @@ const cors = require('cors');
 const view = require('./Component/Controller')
 const app = express();
 // const OFFLINE_DB = "mongodb://localhost/money";
-const ONLINE_DB = 
-"mongodb+srv://dawodu98:joshua98@cluster0.idt4l.mongodb.net/money?retryWrites=true&w=majority"
+const ONLINE_DB = process.env.MONGO_DB_CONNECTION_STRING;
 
 mongoose.connect(ONLINE_DB,{
     useCreateIndex: true,
@@ -23,5 +23,5 @@ app.use(cors())
 
 
 app.listen(port,()=>{
-    console.log(`port is listening`);
+    console.log(`port is listening ${port}`);
 })
